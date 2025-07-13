9326c8ab847440ceb6aca0e46b15f4896fd003af
Annonnymmousss <jatin02012006@gmail.com>
2025-07-13 12:22:13 +0530

    chore : variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let mut x = 5;
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; //constants
+    let mut x = 5; //made mutable
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    //we can change datatype by shadowing but not by making it mutable
+    let spaces = "   ";
+    let spaces = spaces.len();//this is valid
+
+    let mut spaces = "   ";
+    spaces = spaces.len();//this isnt 
 }
