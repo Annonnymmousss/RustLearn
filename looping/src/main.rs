bc5be10d38a7210b66c8ffa44af98c70e953c748
Annonnymmousss <jatin02012006@gmail.com>
2025-07-14 10:34:27 +0530

    chore : looping

---
+++ looping/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+
+//loop
+let mut count = 0;
+'counting_up: loop {     // we can declare such labels to use break or continue as we are allowed to use it in only inner loops
+    println!("count = {count}");
+    let mut remaining = 10;
+    loop {
+        println!("remaining = {remaining}");
+        if remaining == 9 {
+        break;
+    }
+    if count == 2 {
+    break 'counting_up;
+    }
+remaining -= 1;
+}
+count += 1;
+}
+println!("End count = {count}");
+
+
+//while loops
+let mut number = 3;
+while number != 0 {
+    println!("{number}!");
+    number -= 1;
+}
+println!("LIFTOFF!!!");
+
+
+//for loop
+let a = [10, 20, 30, 40, 50];
+for element in a {
+    println!("the value is: {element}");
+}
+}
