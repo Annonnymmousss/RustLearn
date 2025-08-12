04dd16553b5e2ac26b705f97fcd299adcda23207
Annonnymmousss <jatin02012006@gmail.com>
2025-08-12 16:52:10 +0530

    learn :dangling references

--- references/src/main.rs
+++ references/src/main.rs
@@ -86,3 +86,23 @@ fn change(some_string: &mut String) {
 
     let r3 = &mut s; // no problem
     println!("{r3}");
+
+
+//rust never allows Dangling References
+
+
+fn main() {
+    let reference_to_nothing = dangle();
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}// this will throw error because s will be out of scope after dangle() gets over 
+// to make it work 
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}// it give the ownership to someone else
