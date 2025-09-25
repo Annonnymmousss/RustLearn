36f392bbc422353fce90fe3b9a8f753b56fdd331
Annonnymmousss <jatin02012006@gmail.com>
2025-09-25 11:36:50 +0530

    chore : first server

---
+++ first_server/src/main.rs
@@ -0,0 +1,23 @@
+
+use axum::Router;
+use axum::routing::get;
+use axum::response::Html;
+ use std::net::SocketAddr;
+
+#[tokio::main]
+
+
+async fn main(){
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async{Html("HeLLO world")}),
+    );
+
+    let addr = SocketAddr::from(([127,0,0,1],8080));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
+}
+
